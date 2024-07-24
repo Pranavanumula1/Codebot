f84beb660c04c8b8eef71af87ee439e8b06a0aee
Pranavanumula1 <91038429+Pranavanumula1@users.noreply.github.com>
2024-07-23 22:14:31 -0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,26 +1,13 @@
-from flask import Flask, render_template, request
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
 
-app = Flask(__name__)
+# Create a new chat bot named Charlie
+chatbot = ChatBot(
+    'Charlie',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    database_uri='sqlite:///database.sqlite3'
+)
 
-# Initialize the chatbot
-chatbot = ChatBot('ChatterBot', storage_adapter='chatterbot.storage.SQLStorageAdapter')
+# Get a response for some input text
+response = chatbot.get_response('Hello, how are you today?')
+print(response)
 
-# Create a new trainer for the chatbot
-trainer = ChatterBotCorpusTrainer(chatbot)
-
-# Train the chatbot based on the english corpus
-trainer.train('chatterbot.corpus.english')
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/get")
-def get_bot_response():
-    userText = request.args.get('msg')
-    return str(chatbot.get_response(userText))
-
-if __name__ == "__main__":
-    app.run(debug=True)
